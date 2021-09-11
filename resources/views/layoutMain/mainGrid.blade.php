c60c53755c06c039cb7f5a852ac6d42fc0982129
Gabriel fernandes <Gabrielfernandesfotografias@outlook.com>
2021-09-10 23:42:58 -0300

    modsite fist commit

---
+++ resources/views/layoutMain/mainGrid.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        
+
+    </head>
+    <body class="body">
+        <header>
+            <nav>
+                <h1>header</h1>
+            </nav>
+        </header>
+        
+        <aside>
+            <h1>aside</h1>
+        </aside>
+
+        <main>
+            <h1>main</h1>
+        </main>
+        
+        <footer>
+            <h1>footer</h1>
+
+        </footer>
+
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="/css/general.css">
+
+    </head>
+    <body class=" container-fluid body">
+        <header>
+            <nav>
+                <h1>header</h1>
+                <h1>header</h1>
+                <h1>header</h1>
+                <h1>header</h1>
+            </nav>
+        </header>
+        
+        <aside>
+            <h1>aside</h1>
+            <h1>aside</h1>
+            <h1>aside</h1>
+            <h1>aside</h1>
+        </aside>
+
+        <main>
+            <h1>main</h1>
+            <h1>main</h1>
+            <h1>main</h1>
+            <h1>main</h1>
+        </main>
+        
+        <footer>
+            <h1>footer</h1>
+            <h1>footer</h1>
+            <h1>footer</h1>
+
+        </footer>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </body>
+</html>
